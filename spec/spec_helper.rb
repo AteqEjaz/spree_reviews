cd14176f2efb65aaaadbde74b0ca3cf51c8f67fa
Tobias Bohwalli <hi@futhr.io>
2013-06-05 16:47:48 +0200

    Updated spec_helper with new general stuff.
    - Fuubar for spec runs.
    - Simplecov and coveralls.
    - Correct path for testing support.
    - DatabaseCleaner etc.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,26 +1,62 @@
-# Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+if ENV["COVERAGE"]
+  require 'simplecov'
+  require 'coveralls'
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+  SimpleCov.start do
+    add_filter '/spec/'
+    add_group 'Controllers', 'app/controllers'
+    add_group 'Overrides', 'app/overrides'
+    add_group 'Models', 'app/models'
+    add_group 'Views', 'app/views'
+    add_group 'Libraries', 'lib'
+  end
+end
+
+ENV['RAILS_ENV'] = 'test'
+
+require File.expand_path('../dummy/config/environment.rb',  __FILE__)
+
 require 'rspec/rails'
-require 'ffaker'
-
-require 'capybara'
 require 'capybara/rspec'
-require 'capybara/rails'
+require 'ffaker'
+require 'database_cleaner'
 
-Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f }
+Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 
-require 'spree/core/testing_support/factories'
-require 'spree/core/testing_support/controller_requests'
-require 'spree/core/testing_support/authorization_helpers'
-require 'spree/core/url_helpers'
-
-Dir[File.join(File.dirname(__FILE__), "factories/*.rb")].each {|f| require f }
+require 'spree/testing_support/factories'
+require 'spree/testing_support/controller_requests'
+require 'spree/testing_support/authorization_helpers'
+require 'spree/testing_support/url_helpers'
 
 RSpec.configure do |config|
-  config.include Spree::Core::TestingSupport::ControllerRequests
+  config.include Capybara::DSL, type: :request
   config.include FactoryGirl::Syntax::Methods
-  config.include Spree::Core::UrlHelpers
-  config.extend AuthorizationHelpers::Request, :type => :feature
+  config.include Spree::TestingSupport::UrlHelpers
+
+  config.include Spree::TestingSupport::ControllerRequests
+  config.extend Spree::TestingSupport::AuthorizationHelpers::Request, type: :feature
+
+  config.mock_with :rspec
   config.use_transactional_fixtures = false
+  config.fail_fast = ENV['FAIL_FAST'] || false
+
+  # config.before :suite do
+  #   FactoryGirl.factories.clear
+  # end
+
+  config.before do
+    if example.metadata[:js]
+      DatabaseCleaner.strategy = :truncation
+    else
+      DatabaseCleaner.strategy = :transaction
+    end
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
 end
