75a92bceda2782690f36def2acb85e89572f9bef
Tobias Bohwalli <hi@futhr.io>
2013-06-07 05:02:31 +0200

    Cleanup spec.

--- spec/features/admin/reviews_spec.rb
+++ spec/features/admin/reviews_spec.rb
@@ -3,11 +3,6 @@ require 'spec_helper'
 describe "Review Admin", js: true do
   stub_authorization!
 
-  # before do
-  #   @review = create(:review)
-  #   create(:review)
-  # end
-
   let!(:review) { create(:review) }
 
   it "should list out the reviews" do
@@ -26,7 +21,7 @@ describe "Review Admin", js: true do
     visit spree.admin_reviews_path
     page.should have_content(review.title)
     within("tr#review_#{review.id}") { click_link "Edit" }
-    page.should have_content('Editing')
+    page.should have_content("Editing")
     page.should have_content(review.title)
   end
 end
